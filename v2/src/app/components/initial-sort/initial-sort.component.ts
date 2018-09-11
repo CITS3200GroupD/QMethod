477cb99f882eeca71772c5c82873f5d494f61369
Evan Huang <reasonstouninstall+github2@protonmail.com>
2018-09-12 03:45:51 +0800

    Improved init sort CSS+HTML

--- v2/src/app/components/initial-sort/initial-sort.component.ts
+++ v2/src/app/components/initial-sort/initial-sort.component.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-initial-sort',
-  templateUrl: './initial-sort.component.html',
-  styleUrls: ['./initial-sort.component.css']
+  templateUrl: './initial-sort-new.component.html',
+  // templateUrl: './initial-sort.component.html',
+  styleUrls: ['./initial-sort-new.component.css']
+  // styleUrls: ['./initial-sort.component.css']
 })
 export class InitialSortComponent implements OnInit {
 
   statements = [
-    {statement: 'statement 1'},
-    {statement: 'statement 2'},
-    {statement: 'statement 3'},
-    {statement: 'statement 4'},
-    {statement: 'statement 5'},
-    {statement: 'statement 6'},
-    {statement: 'statement 7'},
+    {statement: 'I can see myself fostering understanding between Australia and yyy'},
+    {statement: 'I can see myself enjoying recreational activities with yyy people'},
+    {statement: 'I can see myself speaking yyy better than many other Australians'},
+    {statement: 'I can see myself studying in yyy'},
+    {statement: 'I can see myself taking the WACE exam in yyy'},
+    {statement: 'I can see myself as a more knowledgeable person'},
+    {statement: 'I can see myself able to better understand people from any other culture'},
   ];
 
   disagree = [];
@@ -22,6 +24,8 @@ export class InitialSortComponent implements OnInit {
   agree = [];
 
   onDisagreeDrop(e: any) {
+    console.log(e);
+    this.removeDisagree(e.dragData);   // Fix for bug
     this.disagree.push(e.dragData);
     this.removeStatement(e.dragData);
     this.removeNeutral(e.dragData);
@@ -29,6 +33,7 @@ export class InitialSortComponent implements OnInit {
   }
 
   onNeutralDrop(e: any) {
+    this.removeNeutral(e.dragData);   // Fix for bug
     this.neutral.push(e.dragData);
     this.removeStatement(e.dragData);
     this.removeDisagree(e.dragData);
@@ -36,6 +41,7 @@ export class InitialSortComponent implements OnInit {
   }
 
   onAgreeDrop(e: any) {
+    this.removeAgree(e.dragData);   // Fix for bug
     this.agree.push(e.dragData);
     this.removeStatement(e.dragData);
     this.removeDisagree(e.dragData);
