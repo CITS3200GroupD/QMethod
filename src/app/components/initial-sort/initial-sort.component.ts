f0b7abd5544676abc5456dfea02cc772bf010960
janiceleee <39648139+janiceleee@users.noreply.github.com>
2018-09-20 18:45:19 +0800

    initial-sort with survey api

--- src/app/components/initial-sort/initial-sort.component.ts
+++ src/app/components/initial-sort/initial-sort.component.ts
@@ -1,44 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SurveyService } from '../../survey.service';     // Survey API MW
-import { UserService } from '../../user.service';         // User API MW
+import { SurveyService } from '../../survey.service';     // Survey API
+// import { UserService } from '../../user.service';      // User API
 
 @Component({
   selector: 'app-initial-sort',
-  templateUrl: './initial-sort-new.component.html',
-  // templateUrl: './initial-sort.component.html',
-  styleUrls: ['./initial-sort-new.component.css']
-  // styleUrls: ['./initial-sort.component.css']
+  templateUrl: './initial-sort.component.html',
+  styleUrls: ['./initial-sort.component.css']
 })
 export class InitialSortComponent implements OnInit {
-  /*
-  statements = [
-    { id: 0, string: 'I can see myself fostering understanding between Australia and yyy'},
-    { id: 1, string: 'I can see myself enjoying recreational activities with yyy people'},
-    { id: 2, string: 'I can see myself speaking yyy better than many other Australians'},
-    { id: 3, string: 'I can see myself studying in yyy'},
-    { id: 4, string: 'I can see myself taking the WACE exam in yyy'},
-    { id: 5, string: 'I can see myself as a more knowledgeable person'},
-    { id: 6, string: 'I can see myself able to better understand people from any other culture'},
-  ];
-  */
+  
+  /*statements = [
+    { string: 'I can see myself fostering understanding between Australia and yyy'},
+    { string: 'I can see myself enjoying recreational activities with yyy people'},
+    { string: 'I can see myself speaking yyy better than many other Australians'},
+    { string: 'I can see myself studying in yyy'},
+    { string: 'I can see myself taking the WACE exam in yyy'},
+    { string: 'I can see myself as a more knowledgeable person'},
+    { string: 'I can see myself able to better understand people from any other culture'},
+  ];*/
+  
 
   // error = boolean;                     // TODO: Invalid survey message box on invalid survey id
 
-  id: string;
-  survey: any;
+  id: String;
+  survey: any = {};
 
   // TODO: Change to index display
   current_index = 0;
-  statementObjs: object[] = [];                 // TODO: Store statements as { id: number, string: string }
+  statementObjs: object[] = [];                 // TODO: Store statements as { id: number, string: String }
 
-  disagree: string[] = [];
-  neutral: string[] = [];
-  agree: string[] = [];
+  disagree: String[] = [];
+  neutral: String[] = [];
+  agree: String[] = [];
 
-  disagree_update: number[];
-  neutral_update: number[];
-  agree_update: number[];
+  disagree_update: number[] = [];
+  neutral_update: number[] = [];
+  agree_update: number[] = [];
+
+  TEMP_int_disagree: number[] = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  TEMP_int_neutral: number[] = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  TEMP_int_agree: number[] =  [32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42];
 
   constructor( private route: ActivatedRoute,
     private router: Router,
@@ -48,7 +50,6 @@ export class InitialSortComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.surveyservice.getSurvey(params['id']).subscribe(res => {
         this.survey = res;
-        this.statementObjs = this.survey.statements;    // TODO: Comment out
 
         // TODO: Loop over statements array, record index in { id: __ } and string in {string:___ }
         this.generateStatementsArray();
@@ -58,9 +59,12 @@ export class InitialSortComponent implements OnInit {
 
   // TODO: Loop over statements array, record index in { id: __ } and string in {string:___ }
   private generateStatementsArray() {
-    // this.statements.forEach( (item, index) => { }
+    this.survey.statements.forEach( (item, index) => { 
+      var temp = {id: index, string: item};
+      this.statementObjs.push(temp);
+    });
+
     console.log('generateStatementsArray() called');
-    // console.log(this.statementObjs);
   }
 
   increaseIndex() {
@@ -77,7 +81,8 @@ export class InitialSortComponent implements OnInit {
 
   onDisagreeDrop(e: any) {
     this.removeDisagree(e.dragData);
-    this.disagree.push(e.dragData);   // TODO: push string to disagree and index value to disagree_update
+    this.disagree.push(e.dragData);   
+    this.disagree_update.push(e.dragData.id);
     this.removeStatement(e.dragData);
     this.removeNeutral(e.dragData);
     this.removeAgree(e.dragData);
@@ -85,7 +90,8 @@ export class InitialSortComponent implements OnInit {
 
   onNeutralDrop(e: any) {
     this.removeNeutral(e.dragData);
-    this.neutral.push(e.dragData);  // TODO: push string to disagree and index value to disagree_update
+    this.neutral.push(e.dragData);  
+    this.neutral_update.push(e.dragData.id);
     this.removeStatement(e.dragData);
     this.removeDisagree(e.dragData);
     this.removeAgree(e.dragData);
@@ -93,15 +99,16 @@ export class InitialSortComponent implements OnInit {
 
   onAgreeDrop(e: any) {
     this.removeAgree(e.dragData);
-    this.agree.push(e.dragData); // TODO: push string to disagree and index value to disagree_update
+    this.agree.push(e.dragData); 
+    this.agree_update.push(e.dragData.id);
     this.removeStatement(e.dragData);
     this.removeDisagree(e.dragData);
     this.removeNeutral(e.dragData);
   }
 
   removeStatement(e: any) {
-    // TODO: use item.string == e.string
     this.statementObjs.forEach( (item, index) => {
+      
       if (item == e) { this.statementObjs.splice(index, 1); }
     });
     if (this.current_index >= this.statementObjs.length) {
@@ -111,22 +118,31 @@ export class InitialSortComponent implements OnInit {
 
   removeDisagree(e: any) {
     this.disagree.forEach( (item, index) => {
-      // TODO: use item.string == e.string
-      if (item == e) { this.disagree.splice(index, 1); }
+      
+      if (item == e) { 
+        this.disagree.splice(index, 1); 
+        this.disagree_update.splice(index, 1);
+      }
     });
   }
 
   removeNeutral(e: any) {
     this.neutral.forEach( (item, index) => {
-      // TODO: use item.string == e.string
-      if (item == e) { this.neutral.splice(index, 1); }
+  
+      if (item == e) { 
+        this.neutral.splice(index, 1); 
+        this.neutral_update.splice(index, 1);
+      }
     });
   }
 
   removeAgree(e: any) {
     this.agree.forEach( (item, index) => {
-      // TODO: use item.string == e.string
-      if (item == e) { this.agree.splice(index, 1); }
+
+      if (item == e) { 
+        this.agree.splice(index, 1); 
+        this.agree_update.splice(index, 1);
+      }
     });
   }
 
@@ -135,7 +151,7 @@ export class InitialSortComponent implements OnInit {
   // 2) goes to q-sort page
   publishSortContinue() {
     console.log('publishSortContinue()');
-    // this.userservice.updateUser(TempIntDisagree, TempIntNeutral, TempIntAgree, this.id);
+    // this.userservice.updateUser(this.TEMP_int_disagree, this.TEMP_int_neutral, this.TEMP_int_agree, this.id);
     setTimeout(() => {
       // this.router.navigate(['q-sort']);
       this.router.navigate(['q-sort']);
