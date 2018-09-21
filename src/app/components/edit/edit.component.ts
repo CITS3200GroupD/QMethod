78dc39a6fe66f8c47f13f298fd0c4afea04844f5
Evan Huang <reasonstouninstall+github2@protonmail.com>
2018-09-21 14:59:45 +0800

    Documentation

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
-import { GridTemplates, BlankSurvey, Survey } from '../../models';
-import { SurveyService } from '../../survey.service';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { WindowWrap } from '../../window-wrapper';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import * as Settings from '../../../../config/Settings';
+import { Component, OnInit } from '@angular/core';                      // @ng core
+import { ActivatedRoute, Router } from '@angular/router';               // @ng route
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http'; // @ng HTTP responses
+import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';  // @ng reactive forms
+import { GridTemplates, BlankSurvey, Survey } from '../../models';      // QMd Defines - Models
+import * as Settings from '../../../../config/Settings';                // QMd Settings
+import { SurveyService } from '../../survey.service';                   // QMd Survey Service Middleware
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';                 // ng-bootstrap addon
+import { WindowWrap } from '../../window-wrapper';                      // wrapper for window
 
 @Component({
   selector: 'app-edit',
@@ -14,24 +14,43 @@ import * as Settings from '../../../../config/Settings';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  /** The maximum length of a Survey's Name */
   NAME_LIMIT = Settings.NAME_LIMIT || 100;
+  /** The maximum fields for Questionnaires/Registration */
   FIELDS_LIMIT = Settings.FIELDS_LIMIT || 10;
+  /** The maximum number of statements */
   STATE_LIMIT = Settings.STATE_LIMIT || 80;
+  /** The default Horizontal-X range for the Q-Sort Grid */
   private DEFAULT_RANGE =  Settings.DEFAULT_RANGE || 11;
 
+  /** Pagination variable for current page */
   statements_page: number;
+  /** Flag (set by callback) for grid validity vs number of statements */
   valid_grid: boolean;
+  /*** ng form variable */
   angForm: FormGroup;
 
+  /** survey object (representing survey data) for this component */
   survey: Survey = BlankSurvey;
+  /** Templates for default column values */
   cols_templates = GridTemplates;
+  /** variable for survey's current Horizontal-X range */
   range = this.DEFAULT_RANGE;
+  /** Length variables for display */
   lengths = {
     questionnaire: 0,
     register: 0,
     statements: 0
   };
 
+  /**
+   * Constructor for EditComponent
+   * @param route ng ActivatedRoute
+   * @param router ng Router
+   * @param surveyservice Survey Service Middleware to communicate with express RESTful API server
+   * @param fb FormBuilder to serve dynamic forms
+   * @param window Wrapper for window
+   */
   constructor(private route: ActivatedRoute,
     private router: Router,
     private surveyservice: SurveyService,
@@ -41,6 +60,9 @@ export class EditComponent implements OnInit {
       this.cols_templates = GridTemplates;
     }
 
+  /**
+   * ng reactive form initialisation
+  */
   private createForm(): void {
     this.angForm = this.fb.group({
       survey_id: [{value: '', disabled: true}, Validators.required ],
@@ -49,6 +71,10 @@ export class EditComponent implements OnInit {
     });
   }
 
+  /**
+   * Catch the specified error, print to console and alert the user.
+   * @param error The error to be handled
+   */
   private throwError(error): void {
     try {
       throw new Error(error);
@@ -58,6 +84,10 @@ export class EditComponent implements OnInit {
     }
   }
 
+  /**
+   * Update this survey object's horizontal-x range
+   * @param cols The new horizontal-x value
+   */
   updateRange(range: number): void {
     if (this.survey.publish) {
       this.throwError('Attempted to update a published server');
@@ -67,12 +97,19 @@ export class EditComponent implements OnInit {
     }
   }
 
+  /**
+   * Update this survey object's grid parameters
+   * @param cols The new grid to be set
+   */
   updateGrid(cols: number[]): void {
     if (!this.survey.publish && this.valid_grid) {
       this.survey.cols = cols;
     }
   }
 
+  /**
+   * Callback method for edit-statements subcomponent to set statements & lengths
+   */
   updateStatements(statements: string[]): void {
     if (!this.survey.publish) {
       this.survey.statements = statements;
@@ -80,6 +117,9 @@ export class EditComponent implements OnInit {
     }
   }
 
+  /**
+   * Callback method for edit-forms subcomponent to set fields & lengths
+   */
   updateFields(field: string, item: string[]): void {
     if (!this.survey.publish) {
       this.survey[field] = item;
@@ -87,10 +127,18 @@ export class EditComponent implements OnInit {
     }
   }
 
+  /**
+   * Callback method for edit-grid subcomponent to set valid_grid flag.
+   */
   isInvalidGrid(bool: boolean): void {
     this.valid_grid = bool;
   }
 
+  /**
+   * Update the survey and sync with database
+   * @param name Name of survey
+   * @param range Range of survey
+   */
   updateSurvey(name, range): void {
     if (this.survey.publish) {
       this.throwError('Attempted to update a published server');
@@ -98,13 +146,18 @@ export class EditComponent implements OnInit {
       this.throwError('Invalid Grid');
     } else {
         this.survey.name = name;
-        this.survey.range = range;
-        this.surveyservice.updateSurvey(this.survey)
+        this.survey.range = range;                                                // Change this.survey obj name & range
+        this.surveyservice.updateSurvey(this.survey)                                    // Send this.survey => Survey service middleware updateSurvey() func
           .subscribe( (res: HttpResponse<Object>) => this.successfulUpdate(res, false), // Case 1: Normal HTTP response
                       (err: HttpErrorResponse) => this.failedUpdate(err));              // Case 2: Responded with error
     }
   }
 
+  /**
+   * Send a successful update prompt to user & console
+   * @param res HttpResponse message to display
+   * @param go_home Flag to either re-render or move to admin page
+   */
   private successfulUpdate(res: HttpResponse<Object>, go_home: boolean): void {
     if (this.window.nativeWindow.confirm(`${res}!`)) {
       if (go_home) {
@@ -115,12 +168,20 @@ export class EditComponent implements OnInit {
     }
   }
 
+  /**
+   * Send a failed update message to user & console
+   * @param err Error to print
+   */
   private failedUpdate(err: HttpErrorResponse): void {
     if (this.window.nativeWindow.confirm(`${err.error}`)) {
+      console.error(err);
       this.ngOnInit();
     }
   }
 
+  /**
+   * Publish the current survey (and lock editing)
+   */
   publishSurvey(): void {
     if (!this.valid_grid) {
       this.throwError('Invalid Grid');
@@ -137,8 +198,9 @@ export class EditComponent implements OnInit {
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      // console.log(params);
+      // Get params from ActivatedRoute
       this.surveyservice.getSurvey(params['id']).subscribe(
+        // Survey ID => survey service mw getSurvey() => this.survey
         (res: Survey) => {
           this.survey = res;
           this.lengths.statements = this.survey.statements.length;
