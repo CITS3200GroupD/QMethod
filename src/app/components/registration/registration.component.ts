74d904ed8002f99f623eac5a3f6626d2a89f9b51
Evan Huang <reasonstouninstall+github2@protonmail.com>
2018-09-23 11:54:34 +0800

    Setting up testing

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-registration',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  private TEMP_response: [ "20", "Australian", "Male", "English", "Chinese" ];
+
+  constructor( private route: ActivatedRoute,
+    private router: Router,
+    private userservice: UserService
+    ) { }
+
+  addUser(registration_info) {
+    this.route.params.subscribe(params => {
+      this.userservice.addUser(params['id'], registration_info)
+    });
+  }
 
   ngOnInit() {
   }
