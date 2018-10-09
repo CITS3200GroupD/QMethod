9a19aa06ec8043ba12813bb7e7aff070b58cdf8e
Evan Huang <41961100+evanhwk@users.noreply.github.com>
2018-10-10 00:37:33 +0800

    FormArray Implementation (#59)
    * Q-Sort with drag and drop (#58)
    
    -Statements are fully draggable
    -Changed css so that grid fits page with max range and statements in grid is scrollable
    -Added range to grid
    
    * FormArray implementation for Registration Page
    
    * Readme Update
    
    * Attempted hotfix
    
    
    HF 2
    
    
    HF 3
    
    * HF 2
    
    * ng test
    
    * CF

--- src/app/components/registration/registration.component.spec.ts
+++ src/app/components/registration/registration.component.spec.ts
@@ -1,6 +1,8 @@
+// TODO: Reenable Unit Testing
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule  } from '@angular/router/testing';
 import { RegistrationComponent } from './registration.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MockWindowWrap } from '../../testing/Testing';
 import { WindowWrap } from '../../window-wrapper';
@@ -15,7 +17,12 @@ describe('RegistrationComponent', () => {
         {provide: WindowWrap, useClass: MockWindowWrap}
       ],
       declarations: [ RegistrationComponent ],
-      imports: [ RouterTestingModule, HttpClientModule ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientModule
+      ]
     })
     .compileComponents();
   }));
