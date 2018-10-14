d234603d91d01a34169629bfa9548e5a7367f483
Jia Xun (Jess) <21884866@student.uwa.edu.au>
2018-10-14 19:33:57 +0800

    Moving Modal Template

--- src/app/components/initial-sort/modal-basic-initialsort/modal-basic-initialsort.ts
+++ src/app/components/initial-sort/modal-basic-initialsort/modal-basic-initialsort.ts
@@ -4,7 +4,33 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ngbd-modal-basic-initialsort',
-  templateUrl: './modal-basic-initialsort.html'
+  template: 
+  `<ng-template #content let-modal>
+  <div class="modal-header">
+    <h4 class="modal-title" id="modal-basic-title">Help</h4>
+    <button type="button" class="close" aria-label="Close" (click)="modal.dismiss('Cross click')">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="modal-body">
+
+  <p> You will be given a series of statements. 
+      <br>You will be able to read the statements one by one.
+      <br>Once you read a statement, sort it into one of three given groups that you will see on the screen, depending
+      on whether you agree, you don't agree or you are neutral about it. You can always change your mind later.           
+  </p>
+
+
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-outline-dark" (click)="modal.close()">Ok</button>
+  </div>
+  
+</ng-template>
+
+<button class="btn btn-lg btn-outline-primary" (click)="open(content)">Help</button>
+
+<hr>`
 })
 
 export class NgbdModalBasic {
